ad230787d9dba92ebcae2e13156b4837b91987d5
organictissuemaker <z3na.wu@gmail.com>
2022-10-30 12:59:22 -0700

    getting_started_&_titanic

---
+++ Getting-Started-with-Python-appolecider/about_me.py
@@ -0,0 +1,13 @@
+name = "zena wu"
+pref_name = "zena"
+age = 16
+python_used = True
+hobbies = ["singing", "baking", "playing video games"]
+fav_things = {
+  "movie": "When the Promised FLower Blooms", 
+  "food": "nectarines", 
+  "hobby": hobbies[2],
+  "anime": "Blue Spring Ride"
+}
+
+all_vars = dict(vars())
